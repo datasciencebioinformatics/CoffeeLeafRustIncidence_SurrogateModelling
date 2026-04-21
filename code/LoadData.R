a6f2884d3153ba4e801d0f5ad3b7d99b4aed98dc
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-04-21 14:56:11 -0300

    Create LoadData.R

---
+++ code/LoadData.R
@@ -0,0 +1,8 @@
+# Load the name opf excel sheets
+excel_sheet<-"CLRI_14D"
+                                                                                                                                                    
+# Take the data.frame for this sheet
+df_datapoint<-cbind(time_windows=excel_sheet,data.frame(read_excel(file.path(project_folder, "data/","Coffee_Leaf_Rust_Incidence_modeling_data.xlsx" ), sheet = excel_sheet)))
+
+# Add to data.frame 
+df_datapoints<-rbind(df_datapoints,)
